75e8e28076d135fd537ef79a3a5c8b56f2605fda
Annette Lien <a.lien@posteo.de>
2022-04-11 11:43:18 +0200

    fix: added the genome folders as command line arguments

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
 
-DATABASE="database.db"
+DATABASE="photorhabdus_xenorhabdus.db"
+REF_GENOMES_DIR="actinobacteria_internal streptomyces_internal"
+QUERY_GENOMES_DIR="photorhabdus xenorhabdus"
 
-### First 2 scripts were run locally, because I dont know how to connect the O drive to the server. To use the O drive locally (ubuntu subsystem on win) the drive needs to be mounted: sudo mount -t drvfs O: /mnt/o
+
+### Fist part on local machine, as the query genomes are not on the shared machine. Could be changed, if O drive can be connected to shared machine.
+### to use the O drive locally (ubuntu subsystem on win) the drive needs to be mounted first: sudo mount -t drvfs O: /mnt/o
 
 python -m cProfile -o ../results/profs/01_create_database.prof \
     01_create_database.py -db $DATABASE \
-    > ../results/logs/01_create_database.log 2>&1
+    2>&1 | tee ../results/logs/01_create_database.log
 
-python -m cProfile -o ../results/profs/02_import_genomes.prof \
-    02_import_genomes.py -db $DATABASE \
-    > ../results/logs/02_import_genomes.log 2>&1
+python -m cProfile -o ../results/profs/02_import_genomes_ref.prof \
+    02_import_genomes.py -db $DATABASE -r $REF_GENOMES_DIR \
+    2>&1 | tee ../results/logs/02_import_genomes_ref.log
 
-### Next scripts were run on the server
+
+### Second part on shared machine
+
+python -m cProfile -o ../results/profs/02_import_genomes_query.prof \
+    02_import_genomes.py -db $DATABASE -q $QUERY_GENOMES_DIR \
+    2>&1 | tee ../results/logs/02_import_genomes_query.log
 
 python -m cProfile -o ../results/profs/03_add_core_genome_indicator.prof \
     03_add_core_genome_indicator.py -db $DATABASE \
-    > ../results/logs/03_add_core_genome_indicator.log 2>&1
+    2>&1 | tee ../results/logs/03_add_core_genome_indicator.log
 
 python -m cProfile -o ../results/profs/04_find_approximate_pfamsequences.prof \
-    04_find_approximate_pfamsequences.py -db $DATABASE -t 12 \
-    > ../results/logs/04_find_approximate_pfamsequences.log 2>&1
+    04_find_approximate_pfamsequences.py -db $DATABASE -t 8 \
+    2>&1 | tee ../results/logs/04_find_approximate_pfamsequences.log
 
 python -m cProfile -o ../results/profs/05_cluster_hits.prof \
     05_cluster_hits.py -db $DATABASE -t 8 \
-    > ../results/logs/05_cluster_hits.log 2>&1
+    2>&1 | tee ../results/logs/05_cluster_hits.log
 
 python -m cProfile -o ../results/profs/06_add_cluster_information.prof \
     06_add_cluster_information.py -db $DATABASE \
-    > ../results/logs/06_add_cluster_information.log 2>&1
+    2>&1 | tee ../results/logs/06_add_cluster_information.log
 
 python -m cProfile -o ../results/profs/06_print_results.prof \
     06_print_results.py -db $DATABASE \
-    > ../results/logs/07_print_results.log 2>&1
+    2>&1 | tee ../results/logs/07_print_results.log
--- scripts/run_all_test.sh
+++ scripts/run_all_test.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# to use the O drive locally (ubuntu subsystem on win) the drive needs to be mounted first: sudo mount -t drvfs O: /mnt/o
 
-DATABASE="database_test.db"
+DATABASE="photorhabdus_xenorhabdus_test.db"
+REF_GENOMES_DIR="actinobacteria_internal streptomyces_internal"
+QUERY_GENOMES_DIR="photorhabdus xenorhabdus"
+
+
+### Fist part on local machine, as the query genomes are not on the shared machine
+### to use the O drive locally (ubuntu subsystem on win) the drive needs to be mounted first: sudo mount -t drvfs O: /mnt/o
 
 python -m cProfile -o ../results/profs/01_create_database_test.prof \
     01_create_database.py -db $DATABASE \
     2>&1 | tee ../results/logs/01_create_database_test.log 
 
-python -m cProfile -o ../results/profs/02_import_genomes_test.prof \
-    02_import_genomes.py -db $DATABASE --test \
-    2>&1 | tee  ../results/logs/02_import_genomes_test.log
+python -m cProfile -o ../results/profs/02_import_genomes_ref_test.prof \
+    02_import_genomes.py -db $DATABASE -r $REF_GENOMES_DIR --test \
+    2>&1 | tee  ../results/logs/02_import_genomes_ref_test.log
+
+
+### Second part on shared machine
+
+python -m cProfile -o ../results/profs/02_import_genomes_query_test.prof \
+    02_import_genomes.py -db $DATABASE -q $QUERY_GENOMES_DIR --test \
+    2>&1 | tee  ../results/logs/02_import_genomes_query_test.log
 
 python -m cProfile -o ../results/profs/03_add_core_genome_indicator_test.prof \
     03_add_core_genome_indicator.py -db $DATABASE \
